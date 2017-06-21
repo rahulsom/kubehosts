1421f05aed9a4ebd52a1fe59405356867610184b
Rahul Somasunderam <rsomasunderam@transcendinsights.com>
2017-06-21 15:08:23 -0700

    Fix handling of execute error

--- kubehosts.go
+++ kubehosts.go
@@ -29,10 +29,16 @@ which hostess || brew install hostess
 ### These are domains we know. Hostess can add these to your hosts file
 `
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func renderScript(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	tmpl, err := template.New("kubehosts").Parse(shellHeader)
-	fmt.Fprint(w, tmpl.Execute(w, KubeHost{r.Host}))
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(w, KubeHost{r.Host})
+	if err != nil {
+		panic(err)
+	}
 
 	config, err := rest.InClusterConfig()
 	if err != nil {
@@ -72,7 +78,34 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func renderHealth(w http.ResponseWriter, r *http.Request) {
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	namespaceList, err := clientset.Namespaces().List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	namespaces := namespaceList.Items
+
+	if len(namespaces) < 1 {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "No namespaces found")
+	} else {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "All OK")
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", renderScript)
+	http.HandleFunc("/healthz", renderHealth)
 	http.ListenAndServe(":8080", nil)
 }
